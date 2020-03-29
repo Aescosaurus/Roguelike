be0f49cf2ec3659ddbbadcf75989cb07a74af7b4
Aescosaurus <michaelbuslik@hotmail.com>
2020-03-28 22:53:52 -0700

    Inventory ui start.

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+	:
+	MonoBehaviour
+{
+	public void AddItem( Item item )
+	{
+		items.Add( item );
+		UpdateUI();
+	}
+
+	void UpdateUI()
+	{
+
+	}
+
+	List<Item> items = new List<Item>();
+	public const int itemSlots = 9;
+}
---
+++ Assets/Scripts/Inventory/ItemDragDrop.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragDrop
+	:
+	MonoBehaviour,
+	IPointerDownHandler,
+	IBeginDragHandler,
+	IEndDragHandler,
+	IDragHandler
+{
+	void Start()
+	{
+		rTrans = GetComponent<RectTransform>();
+		origPos = transform.position;
+		canv = FindObjectOfType<Canvas>();
+	}
+
+	public void OnBeginDrag( PointerEventData eventData )
+	{
+		origPos = transform.position;
+	}
+
+	public void OnDrag( PointerEventData eventData )
+	{
+		rTrans.anchoredPosition += eventData.delta / canv.scaleFactor;
+	}
+
+	public void OnEndDrag( PointerEventData eventData )
+	{
+		transform.position = origPos;
+	}
+
+	public void OnPointerDown( PointerEventData eventData )
+	{
+	}
+
+	RectTransform rTrans;
+	Vector3 origPos;
+	Canvas canv;
+}
